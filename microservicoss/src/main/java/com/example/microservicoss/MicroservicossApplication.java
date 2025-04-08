cc73c3393ae21dcdb81efffa5e5a97d84a39cdeb
Caixetaa_ <guilhermecaixeta19@gmail.com>
2025-04-08 13:29:14 -0300

    Add files via upload

---
+++ microservicoss/src/main/java/com/example/microservicoss/MicroservicossApplication.java
@@ -0,0 +1,13 @@
+package com.example.microservicoss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicossApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicossApplication.class, args);
+	}
+
+}
---
+++ microservicoss/src/test/java/com/example/microservicoss/MicroservicossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microservicoss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
